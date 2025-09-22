1798f7f2e900b72422e35b3a7533ed70d2426b5e
Jenilvaghasiya <vaghasiyajenil29@gmail.com>
2025-09-22 10:27:03 +0530

    New project

--- backend/routes/testRoutes.js
+++
@@ -1,9 +0,0 @@
-// backend/routes/testRoutes.js
-const express = require("express");
-const router = express.Router();
-const { insertMessage, getMessages } = require("../Controller/testController");
-
-router.post("/db-check", insertMessage); // POST
-router.get("/db-check", getMessages);   // GET
-
-module.exports = router;
